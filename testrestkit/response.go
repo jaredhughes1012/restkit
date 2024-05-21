47c6cef8501caf0daa9b8aeeee1c373271d8df49
Jared <jaredh1012@gmail.com>
2024-05-20 23:34:49 -0500

    Fixed test json response assert

--- testrestkit/response.go
+++ testrestkit/response.go
@@ -1,19 +1,19 @@
 package testrestkit
 
 import (
-	"net/http/httptest"
+	"net/http"
 	"testing"
 
 	"github.com/jaredhughes1012/restkit"
 	"github.com/stretchr/testify/require"
 )
 
-func RequireJsonResponse[T any](t *testing.T, w *httptest.ResponseRecorder, status int) T {
+func RequireJsonResponse[T any](t *testing.T, res *http.Response, status int) T {
 	t.Helper()
 
 	var data T
-	require.Equal(t, status, w.Code)
-	require.NoError(t, restkit.ReadJsonResponse(w.Result(), &data))
+	require.Equal(t, status, res.StatusCode)
+	require.NoError(t, restkit.ReadJsonResponse(res, &data))
 
 	return data
 }
